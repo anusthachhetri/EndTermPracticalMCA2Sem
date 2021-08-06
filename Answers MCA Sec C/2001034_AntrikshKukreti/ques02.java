d63ac221a889487f6d8d545b0e42592daafc6e44
AntrikshKukreti <85869976+AntrikshKukreti@users.noreply.github.com>
2021-08-06 11:23:42 +0530

    Add files via upload

---
+++ Answers MCA Sec C/2001034_AntrikshKukreti/ques02.java
@@ -0,0 +1,35 @@
+import java.applet.Applet; 
+ import java.awt.*; 
+ import java.awt.event.*; 
+ public class AppletKeyboard extends Applet implements KeyListener 
+   { 
+        TextField t,tt,tp,tr; 
+         public void init() 
+             { 
+                 t=new TextField(20); 
+                 t.addKeyListener(this); 
+                 tt=new TextField(70); 
+                 tp=new TextField(70); 
+                 tr=new TextField(70); 
+                 add(t); 
+                 add(tt); 
+                 add(tr); 
+                 add(tp); 
+             }  
+                 public void keyTyped(KeyEvent e) 
+                      { 
+                          tt.setText("key Released"+e.getKeyChar()); 
+                       }  
+                           public void keyReleased(KeyEvent e) 
+                              { 
+                                  tr.setText("key Released"+e.getKeyChar()); 
+                              }  
+                                  public void keyPressed(KeyEvent e) 
+                                     { 
+                                        int kc; 
+                                      String s; 
+                                      kc=e.getKeyCode(); 
+                                      s=e.getKeyText(kc); 
+                                      tp.setText("Key Pressed"+s); 
+                                   } 
+    }                
